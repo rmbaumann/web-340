2629751c5aa07cab9f11348ec05a3a3dfd3ba082
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-04-21 10:57:59 -0500

    Create baumann-exercise-1.3.js

---
+++ week-1/baumann-exercise-1.3.js
@@ -0,0 +1,24 @@
+/*
+=============================================
+; Title: baumann-exercise-1.3.js
+; Author: Reva Baumann
+; Date: 18 April 2019
+; Modified by: Reva Baumann
+; Description: Recreate the Module Example using URL and query value.
+;============================================
+*/
+
+/*
+Expected output:
+  Reva Baumann
+  Exercise 1.3
+  21 April 2019
+  Expected output
+
+*/
+
+// lists details of formatted header, including first name, last name and assignment
+const header = require('./baumann-header.js');
+console.log(header.display("Reva", "Baumann", "Exercise 1.4"));
+
+// Start Program
