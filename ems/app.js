4b9708c332542311d55ff954998acb9687012a15
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-05-29 18:17:07 -0500

    Update app.js

--- ems/app.js
+++ ems/app.js
@@ -30,22 +30,50 @@ var express = require("express");
 // Declare the http variable and import the http module
 var http = require("http");
 // Declare the path to find views
-var path = require('path');
+var path = require("path");
 // Declare a Morgan logger
 var logger = require("morgan");
 
 var app = express();
 
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("views",path.resolve(__dirname,"views"));
+app.set("view engine","ejs");
 app.use(logger("short"));
-app.get("/", function(req, res) {
-    Response.render("index", {
-        title: "Home Page"
+app.use(express.static('./'));
+
+// Renders the index page
+app.get("/", function(request, response) {
+    response.render("index", {
+        title: "Home Page",
+        page: "home",
+    });
+});
+
+// Renders the list page
+app.get("/list", function(request, response) {
+    response.render("list", {
+        title: "List Employees",
+        page: "list",
+    });
+});
+
+// Renders the new page
+app.get("/new", function(request, response) {
+    response.render("new", {
+        title: "New Employees",
+        page: "new",
+    });
+});
+
+// Renders the View Page
+app.get("/view", function(request, response){
+    response.render("view", {
+        title: "View Employees",
+        page: "view",
     });
 });
 
 // Create Server
 http.createServer(app).listen(8080, function() {
-    console.log("Application started on port 8080!")
-})
+    console.log("Application started on port 8080!");
+});
