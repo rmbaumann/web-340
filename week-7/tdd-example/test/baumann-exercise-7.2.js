cd2bc5b33e14b0658953b50f6939f83f49624485
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-06-05 16:54:58 -0500

    Update baumann-exercise-7.2.js

--- week-7/tdd-example/test/baumann-exercise-7.2.js
+++ week-7/tdd-example/test/baumann-exercise-7.2.js
@@ -34,8 +34,4 @@ describe("String#split", function(){
     it("should return an array of fruits", function(){
         assert(Array.isArray('Apple, Orange, Mango'.split(',')));
     });
-});
-
-function getFruits(str) {
-    return str.split(',');
-}
+})
